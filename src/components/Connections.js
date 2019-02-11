47b3289897d256ef793cfcb43a6b1d3ef5321761
sancarbar <sancarbar@gmail.com>
2019-02-11 18:48:05 -0500

    Added library to create connections graph.

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {UsernameForm} from "./UsernameForm";
+import {NodesGraph} from "./NodesGraph";
 
 
 export class Connections extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {username: ''};
+        this.state = {username: '', resultMessage: "", connections: []};
         this.onSubmitUsername = this.onSubmitUsername.bind(this);
         this.onFindClicked = this.onFindClicked.bind(this);
     }
@@ -16,26 +17,56 @@ export class Connections extends Component {
     }
 
     onFindClicked() {
-        console.log("onFindClicked for username: ", this.state.username)
+        if (this.state.username.length > 3) {
+
+            let context = this
+
+            fetch("https://torre.bio/api/people/" + this.state.username + "/connections")
+
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    if (responseJson.message) {
+                        context.setState({resultMessage: responseJson.message});
+                    } else {
+                        context.setState({connections: responseJson});
+                    }
+                    console.log(responseJson);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
     }
 
-    drawGraph() {
-        
-    }
 
     render() {
 
         let button;
 
         if (this.state.username.length > 3) {
-            button = <button onClick={this.onFindClicked}> Find</button>
+            button = <button onClick={this.onFindClicked}>Find</button>
         }
+
+        let nodes;
+
+        if (this.state.connections.length > 0) {
+            nodes = <NodesGraph username={this.state.username} connections={this.state.connections}/>
+        }
+
 
         return (
             <div>
                 <UsernameForm onSubmitUsername={this.onSubmitUsername}/>
                 <p>Connections for <b>{this.state.username}:</b></p>
                 {button}
+                <br/>
+                {this.state.resultMessage}
+
+                <br/>
+                <br/>
+
+                {nodes}
+
             </div>
         );
     }
---
+++ src/components/NodesGraph.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {InteractiveForceGraph, ForceGraphNode, ForceGraphLink} from 'react-vis-force';
+
+export class NodesGraph extends Component {
+
+    render() {
+
+        let nodes
+
+        nodes = this.props.connections.map((connection, index) =>
+            [<ForceGraphNode key={index} node={{id: index + '-node', label: connection.person.name}} fill="blue"/>,
+            <ForceGraphLink link={{ source: index + '-node' , target: 'user-node' }} />]
+        );
+
+
+        return (
+            <InteractiveForceGraph
+                simulationOptions={{height: 1000, width: 1000}}
+                labelAttr="label"
+                onSelectNode={(node) => console.log(node)}
+                highlightDependencies>
+
+                <ForceGraphNode node={{id: 'user-node', label: this.props.username}} fill="orange"/>
+
+                {nodes}
+
+            </InteractiveForceGraph>
+        );
+    }
+
+
+}
